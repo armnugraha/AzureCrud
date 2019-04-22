4e94854d9b06e074981d9d9bb130f1c9da25ee50
arman nugraha <armannugraha85@gmail.com>
2019-04-22 22:49:03 +0700

    imporve style with logic

--- index.php
+++ index.php
@@ -1,30 +1,40 @@
 <html>
  <head>
- <Title>Registration Form</Title>
- <style type="text/css">
- 	body { background-color: #fff; border-top: solid 10px #000;
- 	    color: #333; font-size: .85em; margin: 20; padding: 20;
- 	    font-family: "Segoe UI", Verdana, Helvetica, Sans-Serif;
- 	}
- 	h1, h2, h3,{ color: #000; margin-bottom: 0; padding-bottom: 0; }
- 	h1 { font-size: 2em; }
- 	h2 { font-size: 1.75em; }
- 	h3 { font-size: 1.2em; }
- 	table { margin-top: 0.75em; }
- 	th { font-size: 1.2em; text-align: left; border: none; padding-left: 0; }
- 	td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
- </style>
+    <Title>Registration Form</Title>
+
+     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
  </head>
  <body>
- <h1>Register here!</h1>
- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p>
+ 
+ <h1 id="logo">Register here!</h1>
+ <!-- <p>Fill in your name and email address, then click <strong>Submit</strong> to register.</p> -->
+
+
  <form method="post" action="index.php" enctype="multipart/form-data" >
-       Name  <input type="text" name="name" id="name"/></br></br>
-       Email <input type="text" name="email" id="email"/></br></br>
-       Job <input type="text" name="job" id="job"/></br></br>
-       <input type="submit" name="submit" value="Submit" />
-       <input type="submit" name="load_data" value="Load Data" />
+       <label> Name </label>  <input type="text" name="name" id="name"/></br></br>
+       <label> Email </label> <input type="text" name="email" id="email"/></br></br>
+       
+       <div class="wrap">
+          <div style="display: inline-flex;">
+            <label for="radio11">Pria</label>
+            <input type="radio" name="jk" checked id="radio11" class="radio" value="pria" />
+
+            <label for="radio12" class="mg-left-8">Perempuan</label>
+            <input type="radio" name="jk" id="radio12" class="radio" value="perempuan" />
+            
+          </div>
+        </div>
+
+        <label> Alamat </label>
+        <textarea placeholder="Alamat" rows="20" name="alamat" id="comment_text" cols="40" class="ui-autocomplete-input" autocomplete="off" role="textbox" aria-autocomplete="list" aria-haspopup="true"></textarea>
+
+       <div style="display: inline-flex;">
+           <input type="submit" name="submit" value="Submit" />
+           <input type="submit" name="load_data" value="Load Data" style="margin-left: 16px" />
+           
+       </div>
  </form>
+
  <?php
     $host = "idappserver.database.windows.net";
     // $host = "tcp:idappserver.database.windows.net:1433";
@@ -42,12 +52,18 @@
     if (isset($_POST['submit'])) {
         try {
             $name = $_POST['name'];
+            $email = $_POST['email'];
+            $jk = $_POST['jk'];
+            $alamat = $_POST['alamat'];
             $date = date("Y-m-d");
             // Insert data
-            $sql_insert = "INSERT INTO User (name) 
-                        VALUES (?)";
+            $sql_insert = "INSERT INTO [dbo].[register] (name, email, jk, alamat) 
+                        VALUES (?,?,?,?)";
             $stmt = $conn->prepare($sql_insert);
             $stmt->bindValue(1, $name);
+            $stmt->bindValue(2, $email);
+            $stmt->bindValue(3, $jk);
+            $stmt->bindValue(4, $alamat);
             $stmt->execute();
         } catch(Exception $e) {
             echo "Failed: " . $e;
@@ -60,11 +76,17 @@
             $stmt = $conn->query($sql_select);
             $registrants = $stmt->fetchAll(); 
             if(count($registrants) > 0) {
-                echo "<h2>People who are registered:</h2>";
+                echo "<caption>People who are registered:</caption>";
                 echo "<table>";
                 echo "<tr><th>Name</th>";
+                echo "<tr><th>Email</th>";
+                echo "<tr><th>Jenis Kelamin</th>";
+                echo "<tr><th>Alamat</th>";
                 foreach($registrants as $registrant) {
                     echo "<tr><td>".$registrant['name']."</td>";
+                    echo "<td>".$registrant['email']."</td>";
+                    echo "<td>".$registrant['jk']."</td>";
+                    echo "<td>".$registrant['alamat']."</td></tr>";
                 }
                 echo "</table>";
             } else {
